ee4a8cd42653d22bee8668541dd02fb42fbab1ef
unknown <brandonlschaen@gmail.com>
2020-08-08 21:21:25 -0400

    Created then function for fetch request

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let url = "https://pokeapi.co/api/v2/ability";
 fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => console.log(data))
+    .then(function(data) {
+
+    })
