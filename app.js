f0636c1c5ff7e53f10bde0a904607f27bd98af73
unknown <brandonlschaen@gmail.com>
2020-08-08 21:25:15 -0400

    Created function for appending an element to another

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 function createNode(element) {
     return document.createElement(element);
 }
+function append(parent, el) {
+    return parent.appendChild(el);
+}
 
 let url = "https://pokeapi.co/api/v2/ability";
 fetch(url)
