567e07db504da8ca11d05680292f8be9a4d1576f
unknown <brandonlschaen@gmail.com>
2020-08-08 21:24:29 -0400

    Created function for creating elements

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
 let url = "https://pokeapi.co/api/v2/ability";
 fetch(url)
     .then(response => response.json())
     .then(data => console.log(data))
     .then(function(data) {
-
+        
     })
