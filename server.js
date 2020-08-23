34c60133a88ac9b504765e21b0f3f683ea308892
KamajiiSC <brandonlschaen@gmail.com>
2020-08-23 18:01:07 -0400

    Changed server to listen to enviromentPort or 3000

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('static', express.static(path.join(__dirname, 'public')));
 
-app.listen(3000)
-console.log('listening to port 3000');
+app.listen(process.env.PORT || 3000)
+console.log('listening');
