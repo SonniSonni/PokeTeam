40c71e04267e37b0a6533a706015ec1d932c1e9c
KamajiiSC <brandonlschaen@gmail.com>
2020-08-23 17:41:59 -0400

    Moved files that dont need to be served into parent directory. Set server to listen on port 3000 and log that its listening

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ var app = express();
 app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('static', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000)
+console.log('listening to port 3000');
